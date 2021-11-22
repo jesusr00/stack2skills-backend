cf93f028bd1c187f7e3d3c2099a48d21feacb513
yusei <acmnhc@gmail.com>
2021-11-22 15:56:59 -0500

    Authentication with google finished

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GoogleAuthModule} from './api/google/google.module';
+import { GoogleStrategy } from './startegys/google.strategy';
+import { AppConfig } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [GoogleAuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GoogleStrategy, AppConfig],
 })
 export class AppModule {}
---
+++ src/config/config.class.ts
@@ -0,0 +1,8 @@
+export class Config{
+    public NODE_ENV: string;
+    public GOOGLE_CLIENT_ID: string;
+    public  GOOGLE_SECRET: string;
+    public URL: string;
+
+    constructor(){}
+}
