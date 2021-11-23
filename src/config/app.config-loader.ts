6e34dec278192c71554497383e33f33e2a4db363
Arael D. Espinosa <cl8dep@gmail.com>
2021-11-23 14:44:32 -0500

    Throw error when config.yaml file not found

--- src/config/app.config-loader.ts
+++ src/config/app.config-loader.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readFileSync, existsSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
 
@@ -6,9 +6,16 @@ const YAML_CONFIG_FILENAME = 'config.yaml';
 
 export default () => {
   const path = join(__dirname, '..', '..');
-  const loaded = yaml.load(
-    readFileSync(join(path, YAML_CONFIG_FILENAME), 'utf8'),
-  ) as Record<string, any>;
+  const filePath = join(path, YAML_CONFIG_FILENAME);
+
+  if (!existsSync(filePath)) {
+    throw new Error(`${YAML_CONFIG_FILENAME} not found`);
+  }
+
+  const loaded = yaml.load(readFileSync(join(filePath), 'utf8')) as Record<
+    string,
+    any
+  >;
 
   return loaded;
 };
