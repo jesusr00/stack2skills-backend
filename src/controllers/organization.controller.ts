5c5b081ea16191ec3ccf6b56515460ef9baec9e1
Arael D. Espinosa Pérez <cl8dep@gmail.com>
2022-01-31 16:35:26 -0500

    Add Open API specification

--- src/controllers/organization.controller.ts
+++ src/controllers/organization.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiResponse, ApiOperation } from '@nestjs/swagger';
 import { OrganizationService } from '~/database/entities';
 import Organization from '~/database/entities/organization/organization.entity';
 
@@ -7,18 +8,25 @@ class OrganizationController {
   constructor(private organizationServices: OrganizationService) {}
 
   @Get()
+  @ApiOperation({ summary: 'List all organizations' })
   GetAll(): Promise<Organization[]> {
     return this.organizationServices.findAll();
   }
 
   @Get('/:id')
+  @ApiOperation({ summary: 'Get an organization by id' })
   findById(@Param('id') id: string): Promise<Organization> {
     return this.organizationServices.findbyId(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create an organization' })
+  @ApiResponse({
+    status: 201,
+    description: 'The organization has been successfully created.',
+  })
   create(@Body() body): Promise<Organization> {
-    const newRepository = body;
+    let newRepository = body;
     return this.organizationServices.create(newRepository);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config';
 
@@ -8,6 +9,14 @@ async function bootstrap() {
   const config = app.get(AppConfigService);
 
   app.enableCors();
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Stack2Skills')
+    .setDescription('Stack2Skills API documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(process.env.PORT || config.http.port, config.http.host);
 }
